09f5d24e93f11ca85c8298ab0890ee35cb04f0a0
Olvet7 <volya.maslova@gmail.com>
2024-04-12 14:33:34 +0300

    1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,64 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random()
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    protected tenants: Person[]
+
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+        this.tenants = [];
+    }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log(`${person.getKey().getSignature()} вертається додому`);
+        } else {
+            console.log('Двері зачинені, спробуйте пізніше');
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+
+    openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()){
+            this.door = true;
+        } else {
+            console.log('Ключ не вірний');
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
